bc468dac145bf5fa2a611fd7def3e343823ed2be
Dima <formyblogger69@gmail.com>
2020-04-10 12:16:15 +0300

    Adding slim gem and configure generators

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,15 @@ module StackoverflowClone
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework :rspec,
+                        fixtures: true,
+                        view_spec: false,
+                        helper_spec: false,
+                        routing_spec: false,
+                        request_spec: false,
+                        controller_spec: true
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    end
   end
 end
