1660277188441307dd336b592c035a5b9b4a0d2c
Gotoinc <root@gotoinc.co>
2020-06-01 14:53:32 +0300

    Change facebook creds to env

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -259,7 +259,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, Rails.application.credentials.facebook[:application_id], Rails.application.credentials.facebook[:application_secret], scope: [:public_profile, :email]
+  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], scope: [:public_profile, :email]
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
