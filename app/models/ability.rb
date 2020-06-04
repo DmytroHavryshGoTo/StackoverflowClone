e8b5254a05cef0015fb851b9acd91b209d12c7c7
Gotoinc <root@gotoinc.co>
2020-06-04 12:55:44 +0300

    Fixed ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -21,11 +21,15 @@ class Ability
 
   def user_ability
     guest_ability
-    can :create, [Question, Answer]
+    can :create, [Question, Answer, Attachment]
     can :update, [Question, Answer], user: user
     can :destroy, [Question, Answer], user: user
     can :mark_best, Answer do |answer|
       user == answer.question.user
+    end
+
+    can [:destroy, :update], Attachment do |attch|
+      user == attch.attachable.user 
     end
   end
 
